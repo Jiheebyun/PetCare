54ca933fc0d54be4a5607a00c8bcd20019204e7f
Jihee Byun <jh910805jh@gmail.com>
2024-02-19 22:31:09 +0900

    [FEAT] FE : fixed adapt page's outline

--- pet-care/src/app/(adaption)/adapt/[id]/page.tsx
+++ pet-care/src/app/(adaption)/adapt/[id]/page.tsx
@@ -2,16 +2,18 @@
 
 "use client"
 
+
+""
 import React from "react";
 import { useRouter } from "next/router";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useSelectedLayoutSegment } from "next/navigation";
 
 import classes from "./page.module.css";
 import Image from "next/image";
 
-export default function listDatail({params}:any){
-    const KEY = params
-    console.log(KEY)
+export default function listDatail({ params }: { params: string }){
+    const KEY = params;
+    console.log(KEY);
 
     return (
         <>
@@ -19,13 +21,25 @@ export default function listDatail({params}:any){
                 <div className={classes.listDatailHeaderContainer}>
                     <h2>활발하고 사람들에게 순한 코코</h2>
                     <div className={classes.heartContainer}>
-                        <Image src={"/img/listDetail/emptyHeart.png"} width={17} height={17} alt="heart"></Image>
+                        <Image
+                            src={"/img/listDetail/emptyHeart.png"}
+                            width={17}
+                            height={17}
+                            alt="heart"
+                        ></Image>
                         <span>저장</span>
                     </div>
                 </div>
+                <div className={classes.imageWrapper}>
+                    <div className={classes.leftImgContainer}>
+                        
+                    </div>
+                    <div className={classes.midImgContainer}>
 
-                <div>
-                    <div> <h1> 이미지 부분</h1></div>
+                    </div>
+                    <div className={classes.rightImgContainer}>
+
+                    </div>
                 </div>
            </div>
         </>
