a2e30742fbef730557b601fa27874c46e7fcc472
jihyeyoon3872 <147459328+jihyeyoon3872@users.noreply.github.com>
2024-02-23 00:22:30 +0900

    [FEAT] FE : cummu

--- pet-care/src/app/(commu)/commu/page.tsx
+++ pet-care/src/app/(commu)/commu/page.tsx
@@ -1,53 +1,69 @@
-"use client"
+"use client";
 import React from 'react';
 import Link from "next/link";
 import styles from "./page.module.css";
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import PetsIcon from '@mui/icons-material/Pets';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 
 interface CommunityProps {
     freeBoardItems: { id: number, title: string }[];
 }
 
-export default function Community( { freeBoardItems} :CommunityProps){
+export default function Community({ freeBoardItems }: CommunityProps) {
     return (
         <div className={styles.community}>
             <section className={styles.freeBoard}>
-                <h2> <Link href="">자유게시판</Link></h2>
-                <ul>
-                    {/* {freeBoardItems.map(item => (
-                        <li key={item.id}>
-                            <Link href={`/freeBoard/${item.id}`}>
-                                <a className={styles.link}>
-                                    {item.title}
-                                </a>
-                            </Link>
-                        </li>
-                    ))} */}
-<li>dd
-    <Link href={''}>dd
-    </Link>
-</li>
-
-                </ul>
+                <h2><PetsIcon/><Link href="">자유게시판</Link><a>더보기 {'>'}</a></h2>
+                <div className={styles.freeBoardTable}>
+                    <a>뿡빵이 자랑해요</a>
+                    <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
+                    <a className={styles.good}><ThumbUpOffAltIcon/>5</a>
+                </div>
+                <div className={styles.freeBoardTable}>
+                    <a>우리 3냥이 아깽이일때</a>
+                    <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
+                    <a className={styles.good}><ThumbUpOffAltIcon/>5</a>
+                </div>
+                <div className={styles.freeBoardTable}>
+                    <a>빌라안 유기묘집 설치 캣맘 대처 어떻게 하나요?</a>
+                    <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
+                    <a className={styles.good}><ThumbUpOffAltIcon/>5</a>
+                </div>
+                <div className={styles.freeBoardTable}>
+                    <a>빌라안 유기묘집 설치 캣맘 대처 어떻게 하나요?</a>
+                    <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
+                    <a className={styles.good}><ThumbUpOffAltIcon/>5</a>
+                </div>
+                <div className={styles.freeBoardTable}>
+                    <a>빌라안 유기묘집 설치 캣맘 대처 어떻게 하나요?</a>
+                    <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
+                    <a className={styles.good}><ThumbUpOffAltIcon/>5</a>
+                </div>
+                <div className={styles.freeBoardTable}>
+                    <a>빌라안 유기묘집 설치 캣맘 대처 어떻게 하나요?</a>
+                    <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
+                    <a className={styles.good}><ThumbUpOffAltIcon/>5</a>
+                </div>
             </section>
-
-            <section className={styles.news}>
-                <h2>최신 뉴스</h2>
-                <ul>
-                    {/* {newsItems.map(item => (
-                        <li key={item.id}>
-                            <Link href={`/news/${item.id}`}>
-                                <a className={styles.link}>
-                                    {item.title}
-                                </a>
-                            </Link>
-                        </li>
-                    ))} */}
-<li>dd  
-    <Link href={''}>dd
-    </Link>
-</li>
-
-                </ul>
+            <section className={styles.freeBoard}>
+                <h2><LibraryBooksIcon/><Link href="">펫케어 뉴스</Link><a>더보기 {'>'}</a></h2>
+                <div className={styles.freeBoardTable}>
+                    <a>뿡빵이 자랑해요</a>
+                    <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
+                    <a className={styles.good}><ThumbUpOffAltIcon/>5</a>
+                </div>
+                <div className={styles.freeBoardTable}>
+                    <a>우리 3냥이 아깽이일때</a>
+                    <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
+                    <a className={styles.good}><ThumbUpOffAltIcon/>5</a>
+                </div>
+                <div className={styles.freeBoardTable}>
+                    <a>빌라안 유기묘집 설치 캣맘 대처 어떻게 하나요?</a>
+                    <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
+                    <a className={styles.good}><ThumbUpOffAltIcon/>5</a>
+                </div>
             </section>
         </div>
     );
