ccdab565a0324d7a0406e529a9fdc427b2d5d7ed
Jihyeyoon <jenny@mustg.kr>
2024-02-24 17:50:04 +0900

    [FEAT] FE : cummu

--- pet-care/src/app/(commu)/commu/page.tsx
+++ pet-care/src/app/(commu)/commu/page.tsx
@@ -6,6 +6,7 @@ import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 import PetsIcon from '@mui/icons-material/Pets';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import Image from "next/image";
 
 interface CommunityProps {
     freeBoardItems: { id: number, title: string }[];
@@ -49,6 +50,73 @@ export default function Community({ freeBoardItems }: CommunityProps) {
             </section>
             <section className={styles.freeBoard}>
                 <h2><LibraryBooksIcon/><Link href="">펫케어 뉴스</Link><a>더보기 {'>'}</a></h2>
+                <ul className={styles.news}>
+                    <li className={styles.newsList}>
+                        <a>
+                            <div>
+                                <span><strong>뿡빵이 자랑해요</strong></span>
+                                <div className={styles.newsListDate}>2024.02.15</div>
+                            </div>
+                            <div className={styles.newListImg}><Image 
+                                src={'/img/dog.png'} 
+                                width={80} 
+                                height={80} 
+                                alt="pet"
+                                property=""
+                            ></Image></div>
+                        </a>
+                    </li>
+                    <li className={styles.newsList}>
+                        <a>
+                            <div>
+                                <span><strong>뿡빵이 자랑해요</strong></span>
+                                <div className={styles.newsListDate}>2024.02.15</div>
+                            </div>
+                            <div className={styles.newListImg}><Image 
+                                src={'/img/dog.png'} 
+                                width={80} 
+                                height={80} 
+                                alt="pet"
+                                property=""
+                            ></Image></div>
+                        </a>
+                    </li>
+                    <li className={styles.newsList}>
+                        <a>
+                            <div>
+                                <span><strong>뿡빵이 자랑해요</strong></span>
+                                <div className={styles.newsListDate}>2024.02.15</div>
+                            </div>
+                            <div className={styles.newListImg}><Image 
+                                src={'/img/dog.png'} 
+                                width={80} 
+                                height={80} 
+                                alt="pet"
+                                property=""
+                            ></Image></div>
+                        </a>
+                    </li>
+                    <li className={styles.newsList}>
+                        <a>
+                            <div>
+                                <span><strong>뿡빵이 자랑해요</strong></span>
+                                <div className={styles.newsListDate}>2024.02.15</div>
+                            </div>
+                            <div className={styles.newListImg}><Image 
+                                src={'/img/dog.png'} 
+                                width={80} 
+                                height={80} 
+                                alt="pet"
+                                property=""
+                            ></Image></div>
+                        </a>
+                    </li>
+
+
+                </ul>
+
+
+{/*                 
                 <div className={styles.freeBoardTable}>
                     <a>뿡빵이 자랑해요</a>
                     <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
@@ -63,7 +131,7 @@ export default function Community({ freeBoardItems }: CommunityProps) {
                     <a>빌라안 유기묘집 설치 캣맘 대처 어떻게 하나요?</a>
                     <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
                     <a className={styles.good}><ThumbUpOffAltIcon/>5</a>
-                </div>
+                </div> */}
             </section>
         </div>
     );
