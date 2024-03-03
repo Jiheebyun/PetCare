2b14a58c9d4a4b32891493a8d08fccd83875c39b
Jihyeyoon <jenny@mustg.kr>
2024-03-03 19:30:07 +0900

    [FEAT] FE : cummu - detailContent
    conflict

---
+++ pet-care/src/app/(commu)/commu/news/page.tsx
@@ -0,0 +1,119 @@
+
+import Link from "next/link";
+import styles from "./page.module.css";
+import Image from "next/image";
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+export default function FreeBoard() {
+    return (
+       <div>
+        <section className={styles.freeBoard}>
+        <h2><LibraryBooksIcon/>펫케어 뉴스</h2>
+        <div className={styles.articleList}>
+            <div className={styles.articleListPre} >
+                <div className={styles.tit}>
+                    <h3><Link href="">강아지 자랑</Link></h3>
+                    <p className={styles.preTxt}><Link href="">얼른 귀엽다고 해줘</Link> </p>
+                    <span className={styles.attachImg}>
+                        <Link href="">
+                    <Image 
+                                src={'/img/dog.png'} 
+                                width={80} 
+                                height={80} 
+                                alt="pet"
+                                property=""
+                            ></Image>
+                        </Link></span>
+                </div>
+                <div className={styles.sub}>
+                    <p className={styles.name}></p>
+                    <div className={styles.wrapInfo}>
+                       <Link href="">조회수</Link>
+                       <Link href="">좋아요</Link>
+                      <Link href="">댓글</Link>
+                       <Link href="" className={styles.date}>2024.02.12</Link>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.articleListPre} >
+                <div className={styles.tit}>
+                    <h3><Link href="">강아지 자랑</Link></h3>
+                    <p className={styles.preTxt}><Link href="">얼른 귀엽다고 해줘</Link> </p>
+                    <span className={styles.attachImg}>
+                        <Link href="">
+                    <Image 
+                                src={'/img/dog.png'} 
+                                width={80} 
+                                height={80} 
+                                alt="pet"
+                                property=""
+                            ></Image>
+                        </Link></span>
+                </div>
+                <div className={styles.sub}>
+                    <p className={styles.name}></p>
+                    <div className={styles.wrapInfo}>
+                       <Link href="">조회수</Link>
+                       <Link href="">좋아요</Link>
+                      <Link href="">댓글</Link>
+                       <Link href="" className={styles.date}>2024.02.12</Link>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.articleListPre} >
+                <div className={styles.tit}>
+                    <h3><Link href="">강아지 자랑</Link></h3>
+                    <p className={styles.preTxt}><Link href="">얼른 귀엽다고 해줘</Link> </p>
+                    <span className={styles.attachImg}>
+                        <Link href="">
+                    <Image 
+                                src={'/img/dog.png'} 
+                                width={80} 
+                                height={80} 
+                                alt="pet"
+                                property=""
+                            ></Image>
+                        </Link></span>
+                </div>
+                <div className={styles.sub}>
+                    <p className={styles.name}></p>
+                    <div className={styles.wrapInfo}>
+                       <Link href="">조회수</Link>
+                       <Link href="">좋아요</Link>
+                      <Link href="">댓글</Link>
+                       <Link href="" className={styles.date}>2024.02.12</Link>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.articleListPre} >
+                <div className={styles.tit}>
+                    <h3><Link href="">강아지 자랑</Link></h3>
+                    <p className={styles.preTxt}><Link href="">얼른 귀엽다고 해줘</Link> </p>
+                    <span className={styles.attachImg}>
+                        <Link href="">
+                    <Image 
+                                src={'/img/dog.png'} 
+                                width={80} 
+                                height={80} 
+                                alt="pet"
+                                property=""
+                            ></Image>
+                        </Link></span>
+                </div>
+                <div className={styles.sub}>
+                    <p className={styles.name}></p>
+                    <div className={styles.wrapInfo}>
+                       <Link href="">조회수</Link>
+                       <Link href="">좋아요</Link>
+                      <Link href="">댓글</Link>
+                       <Link href="" className={styles.date}>2024.02.12</Link>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        </section>
+
+       </div>
+    );
+}
+
