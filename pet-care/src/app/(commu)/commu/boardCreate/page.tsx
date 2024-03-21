3b2e1ed51f79db6f42f3030a905b40ff9ce97eee
jihyeyoon3872 <147459328+jihyeyoon3872@users.noreply.github.com>
2024-03-21 21:55:27 +0900

    [FEAT] FE : commu - boardCreate

--- pet-care/src/app/(commu)/commu/boardCreate/page.tsx
+++ pet-care/src/app/(commu)/commu/boardCreate/page.tsx
@@ -14,7 +14,9 @@ export default  function BoardCreate(props:any) {
   const router = useRouter();
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
-
+  const generateUniqueId = () => {
+    return Math.random().toString(36).substring(7);
+  };
   
   const handleSubmit = async (event:any) => {
     event.preventDefault();
@@ -23,15 +25,21 @@ export default  function BoardCreate(props:any) {
     console.log("content:"+content)//ok
     console.log("props:"+JSON.stringify(props))//ok //props:{"params":{},"searchParams":{}}
     try {
-      // 제목과 내용을 서버에 전송
-      const response = await axios.post('/api/boardCreate', { title, content });
+      const id = generateUniqueId();
+      // 서버에 전송(title, content)
+      //HTTP POST 요청을 서버의 /api/boardCreate 엔드포인트로 보냄
+      const response = await axios.post('/api/boardCreate', { id, title, content });
       console.log("response:"+response)
       console.log("response.status:"+response.status)
 
-      // 서버로부터의 응답을 확인하고 성공했을 때만 페이지 이동
+      // -> 서버의 API 라우트로 이동 -> 서버 측에서는 /api/boardCreate 엔드포인트를 처리할 수 있는 코드가 필요 (클라이언트에서 전송된 데이터를 받아 처리하고, 필요에 따라 데이터베이스에 저장하거나 다른 작업을 수행한 후에 클라이언트에게 응답을 전송)
+
+
+      // 서버로부터 응답 받은 후, 성공했을 때만 페이지 이동
       if (response.status === 200) { //x //404
         console.error('response.status === 200');
         router.push('/boardDetail/'); // 클라이언트 사이드 라우팅을 통한 페이지 이동
+        router.push(`/boardDetail/${id}`);
       }
     } catch (error) {
       console.error('Error:', error);
--- pet-care/src/mocks/boardCreate/http.ts
+++ pet-care/src/mocks/boardCreate/http.ts
@@ -6,7 +6,17 @@ import { handlers } from './handlers';
 const app = express();
 const port = 9090;
 
-app.use(cors({ origin: 'http://localhost:3000', optionsSuccessStatus: 200, credentials: true }));
+app.use(cors({ 
+    origin: 'http://localhost:3000', // 허용할 origin
+    optionsSuccessStatus: 200, // Pre-flight 요청에 대한 응답 상태 코드
+    credentials: true // 인증 정보를 포함한 요청 허용
+  }));
+//credentials: 인증 정보를 포함한 요청을 보내기 위함( 브라우저에서 서버로의 요청이 인증 토큰 등의 인증 정보를 포함)
+//서버에서는 Access-Control-Allow-Credentials 헤더를 포함하여 요청에 대한 응답을 반환해야 함
+
 app.use(express.json());
 app.use(createMiddleware(...handlers));
 app.listen(port, () => console.log(`Mock server is running on port: ${port}`));
+// OPTIONS 요청에 대한 처리
+//브라우저는 실제 요청을 보내기 전에 사전 요청을 보내는것
+app.options('*', cors());
