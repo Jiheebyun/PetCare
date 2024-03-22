429e04449512123c8ffc29a3bff1ba7d28363f4c
jihyeyoon3872 <147459328+jihyeyoon3872@users.noreply.github.com>
2024-03-22 23:14:41 +0900

    [FEAT] FE : commu - commu,freeBoard,detailContent dynamic

--- pet-care/src/app/(commu)/commu/page.tsx
+++ pet-care/src/app/(commu)/commu/page.tsx
@@ -7,41 +7,79 @@ import PetsIcon from '@mui/icons-material/Pets';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 import Image from "next/image";
+import postDataJson from '../../_components/tempDataCommu.json';
+
+interface postDataType {
+    // id : number; //오류. json파일엔 id값이 없기때문
+    title : string;
+    content : string;
+    date : string;
+    read:number;
+    cmt:number;
+    like:number;
+   }
+
 export default function FreeBoard() {
+    const postData:postDataType[] = postDataJson;
     return (
        <div>
+
+       
+
           <section className={styles.freeBoard}>
-                <h2><PetsIcon/><Link href="/commu/freeBoard">자유게시판</Link><Link href="/commu/freeBoard">더보기 {'>'}</Link></h2>
+      
+            <h2><PetsIcon/><Link href="/commu/freeBoard">자유게시판</Link><Link href="/commu/freeBoard">더보기 {'>'}</Link></h2>
+            {/* {postData.map((postData:any)=>{ 
+             return(
                 <div className={styles.freeBoardTable}>
                     <a>뿡빵이 자랑해요</a>
                     <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
                     <a className={styles.good}><ThumbUpOffAltIcon/>5</a>
                 </div>
-                <div className={styles.freeBoardTable}>
-                    <a>우리 3냥이 아깽이일때</a>
-                    <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
-                    <a className={styles.good}><ThumbUpOffAltIcon/>5</a>
-                </div>
-                <div className={styles.freeBoardTable}>
-                    <a>빌라안 유기묘집 설치 캣맘 대처 어떻게 하나요?</a>
-                    <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
-                    <a className={styles.good}><ThumbUpOffAltIcon/>5</a>
-                </div>
-                <div className={styles.freeBoardTable}>
-                    <a>빌라안 유기묘집 설치 캣맘 대처 어떻게 하나요?</a>
-                    <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
-                    <a className={styles.good}><ThumbUpOffAltIcon/>5</a>
-                </div>
-                <div className={styles.freeBoardTable}>
-                    <a>빌라안 유기묘집 설치 캣맘 대처 어떻게 하나요?</a>
-                    <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
-                    <a className={styles.good}><ThumbUpOffAltIcon/>5</a>
-                </div>
-                <div className={styles.freeBoardTable}>
-                    <a>빌라안 유기묘집 설치 캣맘 대처 어떻게 하나요?</a>
-                    <a className={styles.bad}><ChatBubbleOutlineIcon />3</a>
-                    <a className={styles.good}><ThumbUpOffAltIcon/>5</a>
-                </div>
+                )})} */}
+                {postData
+                .sort((a, b) => Date.parse(b.date) - Date.parse(a.date)) // 날짜 내림차순 정렬
+                .slice(0, 10) // 상위 10개 항목만 선택
+                .map((postData, index) => (
+                    <div key={index} className={styles.freeBoardTable}>
+                        <Link 
+                        href={{
+                            pathname: `/commu/detailContent/${index+1}`,
+                            query: { id: `${index+1}` },
+                        }}
+                       >{postData.title}</Link>
+                      <Link 
+                        href={{
+                            pathname: `/commu/detailContent/${index+1}`,
+                            query: { id: `${index+1}` },
+                        }}
+                        ><div className={styles.bad}>
+                            <ChatBubbleOutlineIcon />{postData.cmt}
+                        </div>
+                       </Link>
+                        
+                       <Link 
+                        href={{
+                            pathname: `/commu/detailContent/${index+1}`,
+                            query: { id: `${index+1}` },
+                        }}
+                        ><div className={styles.good}>
+                            <ChatBubbleOutlineIcon />{postData.like}
+                        </div>
+                       </Link>
+
+                       <Link 
+                        href={{
+                            pathname: `/commu/detailContent/${index+1}`,
+                            query: { id: `${index+1}` },
+                        }}
+                        ><div className={styles.good}>
+                          {postData.date}
+                        </div>
+                       </Link>
+
+                    </div>
+                ))}
             </section>
         <section className={styles.freeBoard}>
         <h2><LibraryBooksIcon/>펫케어 뉴스</h2>
