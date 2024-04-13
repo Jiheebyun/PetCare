e86a698fd164b77a4e715570bad2e135d67c9731
Jihee Byun <jh910805jh@gmail.com>
2024-04-13 21:57:06 +0900

    [INIT] added MongoDB

---
+++ pet-care/src/utils/database.ts
@@ -0,0 +1,11 @@
+
+import { MongoClient } from "mongodb";
+
+
+const url = ``;
+const options: any = { useNewUrlParser: true };
+// mongodb+srv://<username>:<password>@cluster0.2wxik4s.mongodb.net/
+
+
+
+
