7429670a89e219b25a08cde8a1accd252ca3621c
Jihee Byun <jh910805jh@gmail.com>
2024-04-16 23:15:14 +0900

    [FEAT] working on mongoDB API

--- pet-care/src/utils/database.ts
+++ pet-care/src/utils/database.ts
@@ -5,7 +5,8 @@ import { MongoClient } from "mongodb";
 const url = ``;
 const options: any = { useNewUrlParser: true };
 // mongodb+srv://<username>:<password>@cluster0.2wxik4s.mongodb.net/
-
+//userName: petCareAdmin
+//password: U6Hj23Omax1Mj3oX
 
 
 
